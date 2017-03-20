e8bf0d9b1a85b19d214be999fa6f8871365aff40
Ross Schulman <ross@rbs.io>
2017-03-19 21:12:18 -0400

    Separate content into two React components
    Eventually these should be set by calls to the backend.

--- westwork-bootstrap-client/src/App.js
+++ westwork-bootstrap-client/src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+function QuestionPrompt(props) {
+  return (
+    <div>
+      <h1>Hello, I'm Westwork</h1>
+      <h2>What's your name?</h2>
+    </div>
+  );
+}
+
+function WestworkForm(props) {
+  return (
+    <div>
+      <form action="dummy">
+        <input type="text" placeholder="Name" />
+      </form>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="Westwork-prompt">
-          <h2>Welcome to Westwork</h2>
-          <h2>What's your name?</h2>
+          <QuestionPrompt />
         </div>
         <div className="Westwork-response">
-          <form action="dummy">
-          </form>
+          <WestworkForm />
+        </div>
       </div>
     );
   }
